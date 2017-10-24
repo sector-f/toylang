044798782bc595fd415f87640cee4964e247e037
Adam <sector-f@users.noreply.github.com>
2017-10-23 21:31:17 -0400

    Started to implement AST

--- src/ast.rs
+++ src/ast.rs
@@ -1,12 +1,31 @@
-pub enum AstNode {
-    VarDeclaration(String),
-    VarAssign(String, Expression),
+#[derive(Debug)]
+pub enum Statement {
+    DeclareVar(String),
+    AssignVar(String, Expr),
+    Print(Expr),
 }
 
 #[derive(Debug)]
-pub enum Expression {
-    Num(f64),
-    Str(String),
-    Bool(bool),
-    Array(Vec<Expression>),
+pub enum Expr {
+    Literal(Value),
+    Reference(String),
+    Op(Op, Box<Expr>, Box<Expr>),
+}
+
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Exp
+}
+
+#[derive(Debug)]
+pub enum Value {
+    Int(i64),
+    Float(f64),
+    String(String),
+    Boolean(bool),
+    Array(Vec<Value>),
 }
--- src/parser.rs
+++ src/parser.rs
@@ -2,12 +2,8 @@ include!(concat!(env!("OUT_DIR"), "/grammar.rs"));
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn test_numbers() {
-        assert_eq!(number("5.0"), Ok(5.0));
-        assert_eq!(number("1_234.5"), Ok(1234.5));
-        assert_eq!(number("-7.2"), Ok(-7.2));
+
     }
 }
