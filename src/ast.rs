0601fe9a8c6077fe77280d91bcbd9c81a2dcbf7b
Adam <sector-f@users.noreply.github.com>
2017-10-24 23:55:51 -0400

    Impl Display for Value

--- src/ast.rs
+++ src/ast.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use std::fmt::{Display, Error, Formatter};
+
 #[derive(Debug)]
 pub enum Line {
     Statement(Statement),
@@ -36,4 +39,21 @@ pub enum Value {
     String(String),
     Boolean(bool),
     Array(Vec<Value>),
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let text = match *self {
+            Value::Int(num) => format!("{}", num),
+            Value::Float(num) => format!("{}", num),
+            Value::String(ref string) => format!("{}", string),
+            Value::Boolean(b) => format!("{}", b),
+            Value::Array(ref vec) => {
+                let list = vec.iter().format_with(", ", |item, f| f(&format_args!("{}", item)));
+                format!("[{}]", list)
+            },
+        };
+
+        write!(f, "{}", text)
+    }
 }
